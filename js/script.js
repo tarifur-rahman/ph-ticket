96940932a2bbd4b220f2eb5745e22df5a6e135c1
tarifur-rahman <tarif6938@gmail.com>
2024-02-19 00:23:47 +0600

    completed js first section

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const allBtn = document.getElementsByClassName('A');
 let count = 0;
 for (const btn of allBtn){
     btn.addEventListener('click' , function(e){
+        
         count = count + 1;
         const elementName = e.target.innerText;
         const priceContainer = document.getElementById('price');
@@ -16,11 +17,35 @@ for (const btn of allBtn){
         priceContainer.appendChild(p2);
         priceContainer.appendChild(p3);
 
+        document.getElementById('total-price');
+        const multi = count * 550;
+        document.getElementById('grand-total');
+        const multi2 = count * 550 ;
 
-
+        showDisplay('grand-total' , multi2)
+        showDisplay('total-price' , multi)
         showDisplay("count-tricket" , count);
     })
 }
+
+const btn = document.getElementById('apply-btn');
+btn.addEventListener('click' , function(){
+    const couponCodeElement = document.getElementById('coupon-input');
+    const couponElement = couponCodeElement.value;
+    const couponCode = couponElement.split(' ').join("").toUpperCase()
+    
+    if(couponCode === "NEW15"){
+        
+        const discountElement = document.getElementById('grand-total');
+        
+    }
+    else if( couponCode === "COUPLE20"){
+        console.log('apni 20% coupon paicen');
+    }
+    else{
+        alert('Wrong Coupon code! please input a Right Coupon Code.Thank You');
+    }
+})
 
 
 function showDisplay(id,value){
